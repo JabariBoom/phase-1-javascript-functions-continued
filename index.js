309140cd6e85e3647bdf239e6c8ecc35f6919605
jabaringure@gmail.com <jabaringure@gmail.com>
2024-09-27 13:05:42 +0300

    Completes functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(coolThing = "roller-skate"){
+    return (`This Saturday, I want to ${coolThing}!`)
+}
+saturdayFun();
+
+const mondayWork = function (dailyActivity = "go to the office"){
+    return (`This Monday, I will ${dailyActivity}.`)
+}
+mondayWork();
+
+function wrapAdjective(visualFlare = '*'){
+    return function (encouragingAdjective = 'special') {
+        return `You are ${visualFlare}${encouragingAdjective}${visualFlare}!`
+    }
+}
